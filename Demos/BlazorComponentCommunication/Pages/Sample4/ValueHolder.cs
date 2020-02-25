c8cf3aed08e0d6e1d5a652701f4816b9a158fa38
Kanda, Jiří <kanda@havit.cz>
2020-02-25 14:18:41 +0100

    Initial commit

---
+++ Demos/BlazorComponentCommunication/Pages/Sample4/ValueHolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.BlazorComponentCommunication.Pages.Sample4
+{
+	public class ValueHolder
+	{
+		public int Value { get; set; }
+	}
+}
---
+++ Demos/BlazorComponentCommunication/Pages/Sample5/ValueChanged.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.BlazorComponentCommunication.Pages.Sample5
+{
+	public class ValueChanged
+	{
+		public int Value { get; }
+
+		public ValueChanged(int value)
+		{
+			Value = value;
+		}
+	}
+}
