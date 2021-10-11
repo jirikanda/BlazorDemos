212ebee19627e27f94a53e2a6bbccecb94f246b8
Kanda, Jiří <kanda@havit.cz>
2021-10-11 09:37:56 +0200

    JsInterop + demo

---
+++ Demos/BlazorJSInterop/wwwroot/apptojs-module.js
@@ -0,0 +1,7 @@
+﻿export function myQuestion() {
+	return confirm("Opravdu chcete kliknout na tlačítko?");
+}
+
+export function myConfirmation() {
+	alert("Potvrzeno.");
+}
---
+++ Demos/BlazorJSInterop/wwwroot/apptojs-performance.js
@@ -0,0 +1,14 @@
+﻿export function calculatePrimes(max) {
+	var result = 0;
+	for (var i = 2; i <= max; i++)
+	{
+		var prime = true;
+		for (var j = 2; j < i; j++) {
+			prime = false;
+		}
+		if (prime) {
+			result++;
+		}
+	}
+	return result;
+}
