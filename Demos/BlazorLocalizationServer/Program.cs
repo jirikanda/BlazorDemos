f28b80500a41fffead953e38755b0d4caa4ab65e
Jiří Kanda <kanda@havit.cz>
2024-01-12 17:00:41 +0100

    BlazorLocalizationServer, BlazorLocalizationWebAssembly: Upgrade to .NET 8

--- Demos/BlazorLocalizationServer/Program.cs
+++ Demos/BlazorLocalizationServer/Program.cs
@@ -11,16 +11,41 @@ namespace BlazorLocalizationServer
 {
 	public class Program
 	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
-	}
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddLocalization();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRequestLocalization(new RequestLocalizationOptions()
+                .AddSupportedCultures("cs", "es", "en")
+                .AddSupportedUICultures("cs", "es", "en")
+                .SetDefaultCulture("es-ES"));
+
+            //app.UseRequestLocalization("cs-CZ");
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
 }
