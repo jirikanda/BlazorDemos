9a605324e08dfa2f0d3522b5289126c8c2aa7c5a
Jiří Kanda <kanda@havit.cz>
2024-01-19 13:51:13 +0100

    BlazorLifecycle: Upgrade to .NET 8

--- Demos/BlazorLifecycle/Program.cs
+++ Demos/BlazorLifecycle/Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BlazorLifecycle
 {
@@ -15,14 +9,33 @@ namespace BlazorLifecycle
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
-		}
+			var builder = WebApplication.CreateBuilder(args);
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
+			// Add services to the container.
+			builder.Services.AddRazorPages();
+			builder.Services.AddServerSideBlazor();
+			builder.Services.AddLocalization();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.MapBlazorHub();
+			app.MapFallbackToPage("/_Host");
+
+			app.Run();
+		}
 	}
 }
