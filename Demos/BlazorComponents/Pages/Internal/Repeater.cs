e74a9c0725176d37f10ec090e5728558c557c9cc
Jiří Kanda <kanda@havit.cz>
2024-03-11 14:24:38 +0100

    Vzdělávací okénko "Blazor & komponenty" 03/2024

---
+++ Demos/BlazorComponents/Pages/Internal/Repeater.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorComponents.Pages
+{
+    public class Repeater<TItem> : ComponentBase
+    {
+        [Parameter, Required] public IEnumerable<TItem> Data { get; set; }
+        [Parameter, Required] public RenderFragment<RepeaterItem<TItem>> ItemTemplate { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            int itemIndex = 0;
+            foreach (TItem item in Data)
+            {
+                builder.AddContent(0, ItemTemplate(new RepeaterItem<TItem> { Index = itemIndex++, Value = item }));
+            }
+        }
+    }
+
+    public class RepeaterItem<TItem>
+    {
+        public int Index { get; init; }
+        public TItem Value { get; init; }
+    }
+}
