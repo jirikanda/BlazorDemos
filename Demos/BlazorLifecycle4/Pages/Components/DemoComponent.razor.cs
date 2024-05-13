647c8b77dd408ed298c2aeea2904f8fe5db787e0
Jiří Kanda <kanda@havit.cz>
2024-05-13 15:38:15 +0200

    Reimplementace lifecycle demos (work in progress)

---
+++ Demos/BlazorLifecycle4/Pages/Components/DemoComponent.razor.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Globalization;
+
+namespace BlazorLifecycle4.Pages.Components
+{
+    public sealed partial class DemoComponent : ComponentBase, IDisposable, IAsyncDisposable
+    {
+        [Inject] private ILogger<DemoComponent> Logger { get; set; }
+
+        [Parameter] public int Value { get; set; }
+
+        // použito jen pro přiřazení v Demo04
+        [Parameter] public CultureInfo Culture { get; set; }
+
+		// všechny virtuální metody životního cyklu logují své volání (viditelné v Output okně při debuggingu)
+		// přidány metody Dispose, DisposeAsync (vč. interfaces)        
+
+		public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            LogMethodCall(nameof(SetParametersAsync) + " - start");
+            await base.SetParametersAsync(parameters); // watch: parameters.ToDictionary()
+            LogMethodCall(nameof(SetParametersAsync) + " - end");
+        }
+
+        protected override void OnInitialized()
+        {
+            LogMethodCall(nameof(OnInitialized));
+            base.OnInitialized();
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            LogMethodCall(nameof(OnInitializedAsync));
+            //await Task.Yield();
+            await base.OnInitializedAsync();
+        }
+
+        protected override void OnParametersSet()
+        {
+            LogMethodCall(nameof(OnParametersSet));
+            base.OnParametersSet();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            LogMethodCall(nameof(OnParametersSetAsync));
+            //await Task.Yield();
+            await base.OnParametersSetAsync();
+        }
+
+        protected override bool ShouldRender()
+        {
+            LogMethodCall(nameof(ShouldRender));
+            return base.ShouldRender();
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            LogMethodCall(nameof(OnAfterRender));
+            base.OnAfterRender(firstRender);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            LogMethodCall(nameof(OnAfterRenderAsync));
+            //await Task.Yield();
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        public void Dispose()
+        {
+            LogMethodCall(nameof(Dispose));
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            LogMethodCall(nameof(DisposeAsync));
+            return ValueTask.CompletedTask;
+        }
+
+        private void LogMethodCall(string methodName)
+        {
+            Logger.LogInformation(GetHashCode() + " " + methodName);
+        }
+    }
+}
---
+++ Demos/BlazorLifecycle4/Pages/README.md
@@ -0,0 +1,24 @@
+﻿# Životní cyklus komponent
+
+## Intro
+* K čemu je dobré async/await?
+* Co je to
+    * Task.CompletedTask
+    * Task.FromResult()
+    * Task.Delay(...).
+    * Task.Yield(...).
+
+## Základní podoba životního cyklu
+* Demo1: Základní metody životního cyklu.
+* Demo1Ref: Ukázka zapojení @ref do životního cyklu
+* Demo2: Založení a ukončení instance, pokud již není potřeba (info o Visible="true/false") 
+* Demo3: Volání při změně parametrů. Pokud se parametry nezmění, na komponentě se žádné kroky životního cyklu neodehrávají.
+* Demo4: Ukázat, že DoNothing způsobí přerenderování komponenty
+
+Zapnout async podobu.
+V DemoComponent odkomentovat 3x await...
+
+## Životní cyklus s async/await
+
+* Demo1: Základní metody životního cyklu. Nyní více ShouldRender, BuildRenderTree, AfterRender.
+* Demo5: Ukázat renderování komponenty samotné na základě událostí.
